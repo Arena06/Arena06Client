15b02c1b4097d45389826891cedce7dd0dd48fef
Jake King <imjake9@gmail.com>
2013-11-25 01:34:47 -0800

    Add support for chat
    Chat GUI is totally not based on anything ever.

--- src/main/java/com/assemblr/arena06/client/scenes/GamePanel.java
+++ src/main/java/com/assemblr/arena06/client/scenes/GamePanel.java
@@ -10,6 +10,7 @@ import com.assemblr.arena06.common.utils.Vector2D;
 import com.google.common.collect.ImmutableMap;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.KeyEventDispatcher;
@@ -20,14 +21,17 @@ import java.awt.geom.Point2D;
 import java.awt.image.BufferedImage;
 import java.lang.reflect.InvocationTargetException;
 import java.net.InetSocketAddress;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
 import java.util.Set;
+import java.util.TreeSet;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
+import org.javatuples.Pair;
 
 
 public class GamePanel extends JPanel implements KeyEventDispatcher, KeyListener {
@@ -35,6 +39,18 @@ public class GamePanel extends JPanel implements KeyEventDispatcher, KeyListener
     private static final double INPUT_ACCELERATION = 4000;
     private static final double FRICTION_ACCELERATION = 2000;
     private static final double MAXIMUM_VELOCITY = 400;
+    
+    private static final Font FONT_PRIMARY;
+    static {
+        Font f = null;
+        try {
+            f = Font.createFont(Font.TRUETYPE_FONT, GamePanel.class.getResourceAsStream("/minecraft.ttf"));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            System.exit(-1);
+        }
+        FONT_PRIMARY = f;
+    }
     
     private final Random random = new Random();
     
@@ -50,6 +66,10 @@ public class GamePanel extends JPanel implements KeyEventDispatcher, KeyListener
     private MapGenerator mapGenerator = new RoomGenerator();
     private TileType[][] map;
     private BufferedImage mapBuffer;
+    
+    private boolean chatting = false;
+    private StringBuilder currentChat = new StringBuilder();
+    private TreeSet<Pair<Date, String>> chats = new TreeSet<Pair<Date, String>>();
     
     private Vector2D velocity = new Vector2D();
     
@@ -240,20 +260,26 @@ public class GamePanel extends JPanel implements KeyEventDispatcher, KeyListener
                         sprites.get(spriteId).updateState((Map<String, Object>) packet.get("data"));
                     }
                 }
+            } else if (packet.get("type").equals("chat")) {
+                long timestamp = (Long) packet.get("timestamp");
+                String content = (String) packet.get("content");
+                chats.add(new Pair<Date, String>(new Date(timestamp), content));
             }
         }
         
         Vector2D acceleration = new Vector2D();
         
-        int xInput = (keysDown.contains(KeyEvent.VK_A) ? -1 : 0) + (keysDown.contains(KeyEvent.VK_D) ? 1 : 0);
-        int yInput = (keysDown.contains(KeyEvent.VK_W) ? -1 : 0) + (keysDown.contains(KeyEvent.VK_S) ? 1 : 0);
-        
-        if (xInput == 0 || yInput == 0) {
-            acceleration.x = INPUT_ACCELERATION * xInput;
-            acceleration.y = INPUT_ACCELERATION * yInput;
-        } else {
-            acceleration.x = INPUT_ACCELERATION * xInput / Math.sqrt(2);
-            acceleration.y = INPUT_ACCELERATION * yInput / Math.sqrt(2);
+        if (!chatting) {
+            int xInput = (keysDown.contains(KeyEvent.VK_A) ? -1 : 0) + (keysDown.contains(KeyEvent.VK_D) ? 1 : 0);
+            int yInput = (keysDown.contains(KeyEvent.VK_W) ? -1 : 0) + (keysDown.contains(KeyEvent.VK_S) ? 1 : 0);
+            
+            if (xInput == 0 || yInput == 0) {
+                acceleration.x = INPUT_ACCELERATION * xInput;
+                acceleration.y = INPUT_ACCELERATION * yInput;
+            } else {
+                acceleration.x = INPUT_ACCELERATION * xInput / Math.sqrt(2);
+                acceleration.y = INPUT_ACCELERATION * yInput / Math.sqrt(2);
+            }
         }
         
         velocity.add(Vector2D.multiply(acceleration, delta));
@@ -341,6 +367,45 @@ public class GamePanel extends JPanel implements KeyEventDispatcher, KeyListener
         player.render(g);
         g.translate(-player.getX(), -player.getY());
         
+        // untranslate camera
+        g.translate(-((getWidth() - player.getWidth()) / 2.0 - player.getX()), -((getHeight() - player.getHeight()) / 2.0 - player.getY()));
+        
+        // draw chat
+        g.setFont(FONT_PRIMARY.deriveFont(16f));
+        if (chatting) {
+            g.setColor(new Color(0x88000000, true));
+            g.fillRect(10, getHeight() - 45, getWidth() - 20, 35);
+            if (!chats.isEmpty())
+                g.fillRect(10, getHeight() - (chats.size() * 25 + 65), getWidth() - 20, chats.size() * 25 + 10);
+            
+            g.setColor(Color.WHITE);
+            g.drawString(currentChat.toString() + "_", 20, getHeight() - 20);
+            int i = 1;
+            for (Pair<Date, String> chat : chats.descendingSet()) {
+                g.drawString(chat.getValue1(), 20, getHeight() - (i * 25 + 40));
+                i++;
+            }
+        } else {
+            int i = 1;
+            if (!chats.isEmpty()) {
+                double firstOpacity = 1.0 - (System.currentTimeMillis() - chats.last().getValue0().getTime() - 5000.0) / 1000.0;
+                if (firstOpacity < 0) firstOpacity = 0;
+                else if (firstOpacity > 1) firstOpacity = 1;
+                g.setColor(new Color(0f, 0f, 0f, (float) (0.345 * firstOpacity)));
+                g.fillRect(10, getHeight() - 65, getWidth() - 20, 10);
+            }
+            for (Pair<Date, String> chat : chats.descendingSet()) {
+                double opacity = 1.0 - (System.currentTimeMillis() - chat.getValue0().getTime() - 5000.0) / 1000.0;
+                if (opacity < 0) break;
+                if (opacity > 1) opacity = 1;
+                g.setColor(new Color(0f, 0f, 0f, (float) (0.345 * opacity)));
+                g.fillRect(10, getHeight() - (i * 25 + 65), getWidth() - 20, 25);
+                g.setColor(new Color(1f, 1f, 1f, (float) opacity));
+                g.drawString(chat.getValue1(), 20, getHeight() - (i * 25 + 40));
+                i++;
+            }
+        }
+        
     }
     
     public boolean dispatchKeyEvent(KeyEvent ke) {
@@ -349,11 +414,42 @@ public class GamePanel extends JPanel implements KeyEventDispatcher, KeyListener
     }
     
     public void keyTyped(KeyEvent ke) {
+        if (chatting) {
+            if (ke.getKeyChar() != KeyEvent.CHAR_UNDEFINED && Character.getType(ke.getKeyChar()) != Character.CONTROL) {
+                currentChat.append(ke.getKeyChar());
+            } else {
+                if (ke.getKeyChar() == '\u0008') {
+                    if (currentChat.length() != 0)
+                        currentChat.deleteCharAt(currentChat.length() - 1);
+                }
+            }
+        }
     }
     
     public void keyPressed(KeyEvent ke) {
         if (keysDown.contains(ke.getKeyCode())) return;
         keysDown.add(ke.getKeyCode());
+        
+        if (ke.getKeyCode() == KeyEvent.VK_ENTER) {
+            if (!chatting) {
+                chatting = true;
+            } else {
+                String message = currentChat.toString().trim();
+                if (message.length() != 0) {
+                    client.sendData(ImmutableMap.<String, Object>of(
+                        "type", "chat",
+                        "message", currentChat.toString()
+                    ));
+                }
+                currentChat = new StringBuilder();
+                chatting = false;
+            }
+        } else if (ke.getKeyCode() == KeyEvent.VK_ESCAPE) {
+            if (chatting) {
+                currentChat = new StringBuilder();
+                chatting = false;
+            }
+        }
     }
     
     public void keyReleased(KeyEvent ke) {
